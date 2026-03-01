56a57fd54077fe0c736686e3e03dbca3155b80e6
triveditravels10-prog <triveditravels10@gmail.com>
2026-03-01 08:01:38 +0530

    Add student data and display functionality

---
+++ student.rb
@@ -0,0 +1,2 @@
+students=[{name:"Rohan",class:"10th",achievement:"Math Topper"},{name:"Anjali",class:"9th",achievement:"Debate Champion"}]
+students.each{|s| puts "#{s[:name]} | #{s[:class]} | #{s[:achievement]}"}
